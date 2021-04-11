3bcf0acaff55c528064c9513d22faae80a19cc17
deadlorpa <40848236+deadlorpa@users.noreply.github.com>
2021-04-11 21:09:51 +0700

    Update variant1.erl

--- task1/variant1.erl
+++ task1/variant1.erl
@@ -7,9 +7,4 @@ start() ->
    MainList = [5,6,0,1,2,3],
    Index = str(MainList, Candidate),
    io:fwrite("Does ~w contains ~w?~n",[MainList, Candidate]),
-   if
-	Index > 0 ->
-		io:fwrite("yes");
-	true ->
-		io:fwrite("no")
-end.
+   io:fwrite("~w~n",[Index > 0]).
