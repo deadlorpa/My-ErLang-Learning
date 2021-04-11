df2468d04d350fa254dcfe298d3e1b40817f171e
deadlorpa <40848236+deadlorpa@users.noreply.github.com>
2021-04-11 21:20:07 +0700

    Update variant2.erl

--- task1/variant2.erl
+++ task1/variant2.erl
@@ -5,17 +5,15 @@ subset([],_) -> false;
 subset(List,List) -> true;
 subset(Main, Cand) ->
     LenCand = length(Cand),
-    [H | T] = Main,
     NewSublist = lists:sublist(Main, LenCand),
-    if Cand == NewSublist ->
-        true;
-    true ->
-        subset(T, Cand)
-    end.
+    subset(NewSublist, Cand, Main).
+    
+subset(Cand, Cand, _)-> true;
+subset(_, Cand, [_|T]) -> subset(T,Cand).
 
 start() -> 
    Candidate = [1,2,[3,4],5], 
-   MainList = [5,6,0,1,2,[3,4],53,5,6,78,34,12],
+   MainList = [5,6,0,1,2,[3,4],55,5,6,78,34,12],
    Result = subset(MainList, Candidate),
    io:fwrite("Does ~w contains ~w?~n",[MainList, Candidate]),
    io:fwrite("~w~n",[Result]).
